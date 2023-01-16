9a644c2732b0b2e271660fe43a82e1282cc2e6a3
gabrielemercolino <gmercolino2003@gmail.com>
2023-01-16 11:54:24 +0100

    added "Cardano"

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Corso web development";
+export const SITE_TITLE = "Corso web development Cardano";
 export const SITE_DESCRIPTION = "Sito web del corso";
 export const AUTHOR_NAME = "Gabriele Mercolino";
